377d007083fc8d9581291a3e71738cae47b15dcb
sahin-dev <sahinsiraj360@gmail.com>
2025-11-15 20:21:16 +0600

    setup model layer

---
+++ src/main/java/com/sahinsiraj/ecom/model/AppRole.java
@@ -0,0 +1,7 @@
+package com.sahinsiraj.ecom.model;
+
+public enum AppRole {
+    ROLE_USER,
+    ROLE_SELLER,
+    ROLE_ADMIN
+}
--- src/main/java/com/sahinsiraj/ecom/model/Product.java
+++ src/main/java/com/sahinsiraj/ecom/model/Product.java
@@ -1,43 +1,49 @@
 package com.sahinsiraj.ecom.model;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
-@Table(name = "products")
 @Data
-@AllArgsConstructor
 @NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "products")
 @ToString
-
 public class Product {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long productId;
 
-    private String title;
+    @NotBlank
+    @Size(min = 3, message = "Product name must contain atleast 3 characters")
+    private String productName;
+    private String image;
+
+    @NotBlank
+    @Size(min = 6, message = "Product description must contain atleast 6 characters")
+    private String description;
+    private Integer quantity;
+    private double price;
+    private double discount;
+    private double specialPrice;
 
     @ManyToOne
+    @JoinColumn(name = "category_id")
     private Category category;
 
-    private String description;
+    @ManyToOne
+    @JoinColumn(name = "seller_id")
+    private User user;
 
-    private double price;
-
-    private Integer stocks;
-
-    private Integer availableStocks;
-
-
+    @OneToMany(mappedBy = "product", cascade = {CascadeType.PERSIST, CascadeType.MERGE}, fetch = FetchType.EAGER)
+    private List<CartItem> products = new ArrayList<>();
 }
